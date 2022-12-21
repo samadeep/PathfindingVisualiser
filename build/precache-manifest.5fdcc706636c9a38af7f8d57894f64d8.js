9150ef3b11cb1fca0d94395399d76c79b0667a10
samadeep <samadeepsengupta@gmail.com>
2022-12-21 15:35:22 +0530

    All FILES

---
+++ build/precache-manifest.5fdcc706636c9a38af7f8d57894f64d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a24c62f51db9a3be88e27ec714aa59d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54f1e3fb22109b64af8d",
+    "url": "/static/css/main.a0ab3751.chunk.css"
+  },
+  {
+    "revision": "a6ec96ad38f8534b2dbb",
+    "url": "/static/js/2.f4fe9229.chunk.js"
+  },
+  {
+    "revision": "54f1e3fb22109b64af8d",
+    "url": "/static/js/main.67c21460.chunk.js"
+  },
+  {
+    "revision": "512a4f888a379d4d7645",
+    "url": "/static/js/runtime-main.1638fc71.js"
+  }
+]);
